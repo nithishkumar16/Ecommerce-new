c601c1c3ba1f4e232d351ba9494558bf0bede40e
nithish <nithish6@gmail.com>
2024-08-28 23:48:42 -0700

    Added NewCollection, NewLetter and Footer section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import LoginSignup from "./Pages/LoginSignup";
 import Cart from "./Pages/Cart";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -17,12 +18,13 @@ function App() {
           <Route path="/men" element={<ShopCategory category="men" />} />
           <Route path="/women" element={<ShopCategory category="women" />} />
           <Route path="/kids" element={<ShopCategory category="kids" />} />
-          <Route path='/product' element={<Product/>}>
-            <Route path=':productId' element={<Product/>}/>
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/login' element={<LoginSignup/>}/>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
